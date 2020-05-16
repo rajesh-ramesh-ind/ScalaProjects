e10ce99975dbb3ea5373257c8026512074bf60dc
rajesh <rajeshramesh38@gmail.com>
2020-05-16 10:33:10 +0530

    AddingOddElements

--- src/AddingOddElements.scala
+++ src/AddingOddElements.scala
@@ -1,6 +1,6 @@
 object AddingOddElements {
   def main(args: Array[String]): Unit = {
     val arr = Range(1, 100, 3).toList
-    print(arr.zipWithIndex.filter(_._2 % 2 == 0).map(_._1).sum)
+    print(arr.zipWithIndex.filter(_._2 % 2 != 0).map(_._1).sum)
   }
 }
